a9128f0cc18548fb3ca6205c22d610afe08e0064
Manish Dubey <mandeep.mca2007@gmail.com>
2023-03-10 21:44:40 -0700

    React Learning-Day1-Working With Component

---
+++ react-complete-guide/src/components/Expenses/Expenses.js
@@ -0,0 +1,20 @@
+import ExpenseItem from "./ExpenseItem/ExpenseItem";
+import Card from "../UI/Card/Card";
+import "./Expenses.css";
+const Expenses = (props) => {
+  return (
+    <Card className="expenses">
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+    </Card>
+  );
+};
+export default Expenses;
